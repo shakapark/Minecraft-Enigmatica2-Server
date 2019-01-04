11b4339a4afd2ebf9b2f876feee2f3237e9ae2f1
shakapark <florent.padiou@gmail.com>
2019-01-04 23:32:16 +0100

    Add Some Environment Variables to Configure Server

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,14 @@ if [[ ! -x /home/minecraft/enigmatica2/ServerStartLinux.sh ]]; then
 fi
 
 ## Config via Environment Variables
+sed -i "/motd\s*=.*/ c motd=$MOTD" /home/minecraft/enigmatica2/server.properties
+sed -i "/level-name\s*=.*/ c level-name=$MAP" /home/minecraft/enigmatica2/server.properties
+sed -i "/difficulty\s*=.*/ c difficulty=$DIFFICULTY" /home/minecraft/enigmatica2/server.properties
+sed -i "/gamemode\s*=.*/ c gamemode=$GAMEMODE" /home/minecraft/enigmatica2/server.properties
+sed -i "/max-players\s*=.*/ c max-players=$MAXPLAYERS" /home/minecraft/enigmatica2/server.properties
+sed -i "/pvp\s*=.*/ c pvp=$PVP" /home/minecraft/enigmatica2/server.properties
+sed -i "/view-distance\s*=.*/ c view-distance=$VIEWDISTANCE" /home/minecraft/enigmatica2/server.properties
+sed -i "/hardcore\s*=.*/ c hardcore=$HARDCORE" /home/minecraft/enigmatica2/server.properties
 
 ## Launch Server
 ./ServerStartLinux.sh
